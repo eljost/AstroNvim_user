01c920639cd61ac8b28980a36c1109421fdfe986
Johannes Steinmetzer <josteinm@posteo.de>
2023-03-14 11:06:22 +0100

    add vimux, use Tab/S-Tab for buffer switching
    and "hardcode" python3_host_prog to a certain venv

--- mappings.lua
+++ mappings.lua
@@ -15,6 +15,20 @@ return {
       end,
       desc = "Pick to close",
     },
+    ["<Tab>"] = {
+      function()
+        require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+      end,
+      desc = "Next buffer"
+    },
+    ["<S-Tab>"] = {
+      function()
+        require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+      end,
+      desc = "Previous buffer",
+    },
+    ["<Leader>vp"]  = { "<cmd>VimuxPromptCommand<cr>", desc = "Prompt vimux command" },
+    ["<Leader>vl"]  = { "<cmd>VimuxRunLastCommand<cr>", desc = "Run last vimux command" },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
